e16cb516bd0688100cfc69fdd8987bad4b5b3362
ifdc2308 <italodorea123@gmail.com>
2025-06-27 15:21:21 -0300

    feat-next-vercel-deploy

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // basePath: '/portfolio',        // nome do seu repositório
-  // assetPrefix: '/portfolio',     // necessário para arquivos estáticos
   images: {
-    unoptimized: true                // necessário para next/image no export
+    unoptimized: true      
   },
 }
 
